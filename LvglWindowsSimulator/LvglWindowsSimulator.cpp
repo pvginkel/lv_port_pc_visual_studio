72925a3112b4494e49d9b4821fbffba5ca2e78bc
Pieter van Ginkel <pvginkel@gmail.com>
2024-03-29 08:55:25 +0100

    Reorganized the project.

--- LvglWindowsSimulator/LvglWindowsSimulator.cpp
+++ LvglWindowsSimulator/LvglWindowsSimulator.cpp
@@ -1,79 +1,52 @@
-﻿#include <Windows.h>
+﻿#include "includes.h"
 
 #include <LvglWindowsIconResource.h>
-
-#include "lvgl/lvgl.h"
-#include "lvgl/examples/lv_examples.h"
-#include "lvgl/demos/lv_demos.h"
-
-#include "includes.h"
+#include <Windows.h>
 
 #include "Application.h"
-#include "Device.h"
-#include "Queue.h"
+#include "lvgl/lvgl.h"
 
-int main()
-{
+int main() {
     lv_init();
 
-    int32_t zoom_level = 100;
+    int32_t zoom_level = 50;
     bool allow_dpi_override = false;
     bool simulator_mode = true;
-    lv_display_t* display = lv_windows_create_display(
-        L"LVGL Windows Simulator Display 1",
-        800,
-        480,
-        zoom_level,
-        allow_dpi_override,
-        simulator_mode);
-    if (!display)
-    {
+    lv_display_t* display = lv_windows_create_display(L"Kitchen Display", 1024, 600, zoom_level,
+                                                      allow_dpi_override, simulator_mode);
+
+    if (!display) {
         return -1;
     }
 
     HWND window_handle = lv_windows_get_display_window_handle(display);
-    if (!window_handle)
-    {
+    if (!window_handle) {
         return -1;
     }
 
-    HICON icon_handle = LoadIconW(
-        GetModuleHandleW(NULL),
-        MAKEINTRESOURCE(IDI_LVGL_WINDOWS));
-    if (icon_handle)
-    {
-        SendMessageW(
-            window_handle,
-            WM_SETICON,
-            TRUE,
-            (LPARAM)icon_handle);
-        SendMessageW(
-            window_handle,
-            WM_SETICON,
-            FALSE,
-            (LPARAM)icon_handle);
+    HICON icon_handle = LoadIconW(GetModuleHandleW(NULL), MAKEINTRESOURCE(IDI_LVGL_WINDOWS));
+    if (icon_handle) {
+        SendMessageW(window_handle, WM_SETICON, TRUE, (LPARAM)icon_handle);
+        SendMessageW(window_handle, WM_SETICON, FALSE, (LPARAM)icon_handle);
     }
 
     lv_indev_t* pointer_indev = lv_windows_acquire_pointer_indev(display);
-    if (!pointer_indev)
-    {
+    if (!pointer_indev) {
         return -1;
     }
 
     lv_indev_t* keypad_indev = lv_windows_acquire_keypad_indev(display);
-    if (!keypad_indev)
-    {
+    if (!keypad_indev) {
         return -1;
     }
 
     lv_indev_t* encoder_indev = lv_windows_acquire_encoder_indev(display);
-    if (!encoder_indev)
-    {
+    if (!encoder_indev) {
         return -1;
     }
 
-    //lv_demo_widgets();
-    //lv_demo_benchmark();
+    // lv_demo_widgets();
+    // lv_demo_benchmark();
 
     _putenv("CURL_CA_BUNDLE=../Output/Binaries/Debug/x64/curl-ca-bundle.crt");
 
@@ -81,14 +54,7 @@ int main()
 
     application.begin();
 
-    if (false) {
-        lv_demo_widgets();
-        lv_demo_widgets_start_slideshow();
-    }
-
-
-    while (1)
-    {
+    while (1) {
         application.process();
 
         uint32_t time_till_next = lv_timer_handler();
